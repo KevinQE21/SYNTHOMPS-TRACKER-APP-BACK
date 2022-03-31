c44bc84884e8f8ac1cee1c9477cfc8597ad2a144
Kevin Quesada Elizondo <kevinque01@gmail.com>
2022-03-31 16:28:05 -0600

    Cambios en codigo

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const { AuthController } = require('../controllers');
 const { AuthValidator } = require('../validators');
 const validators = require('celebrate');
+const { verifyToken } = require('../middlewares');
 
 // Register - POST
 router.post('/register', AuthValidator, AuthController.register);
 
 // Login - POST
 router.post('/login', AuthValidator, AuthController.login);
+
+// User Auth - GET
+router.post('/user', verifyToken, AuthController.getUserAuth);
 
 //Manejador de error de validators
 router.use(validators.errors());
